7dc11253a59d9dbfe9d5b7ee67d6847185800f67
kk77777 <kaushikgattani@gmail.com>
2021-05-25 19:51:37 +0530

    added new game functionality

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true }];
+
 const App = () => {
-  const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isXNext: true },
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
 
   const [currentMove, setCurrentMove] = useState(0);
 
@@ -37,11 +37,19 @@ const App = () => {
     setCurrentMove(move);
   };
 
+  const newGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleClick={handleClick} />
+      <button type="button" onClick={newGame}>
+        New Game
+      </button>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
